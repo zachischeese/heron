2054c32af66f6f066100a92589fafeeb21bd03c9
Zach Ashton <zashton36@gmail.com>
2018-01-29 10:22:10 +0000

    First

---
+++ heron.js
@@ -0,0 +1,35 @@
+/*
+OHHH YEAHHHHHH JAVASCRIPT!
+This is honestly possibly my favorite language as I can do crap like this.
+I can also make stuff like TomBot and Saori-Chan with it (node things for discord).
+Ohhh yeaaahhhhhhh baby!
+*/
+document.getElementById("modal").style.display = "none";
+
+$(document).mousemove(function(e){
+    $("#heron").css({left:e.pageX-50, top:e.pageY-50});
+});
+
+var screenWidth = ($(document).width() - 300);
+var screenHeight = ($(document).height() - 300);
+
+$(function(){
+    $(".pan").on({
+        mouseover:function(){
+            $(this).css({
+                left:(Math.random()*screenWidth)+"px",
+                top:(Math.random()*screenHeight)+"px",
+            });
+        }
+    });
+});
+
+function buttonClick()
+{
+	document.getElementById("modal").style.display = "block";
+}
+
+function hideMe()
+{
+	document.getElementById("modal").style.display = "none";
+}
